04e9a5daab93748777bcf595c9dd505063ef980d
EfeAdzhar <efeadzhar@gmail.com>
2021-10-13 13:42:39 +0300

    first commit

---
+++ hw_1_base_operations/src/main/java/ua/com/alevel/BaseOperations1.java
@@ -0,0 +1,31 @@
+package ua.com.alevel;
+import java.util.Scanner;
+
+public class BaseOperations1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = "";
+        System.out.println("A program that takes a string\n" +
+                "and extracts all the numbers\n" +
+                "and finds their sum");
+        System.out.println("Enter your string:");
+        line = scanner.nextLine();
+        System.out.println("Your output: " + findSum(line));
+    }
+    static int findSum(String str) {
+        String temp = "0";
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char character = str.charAt(i);
+            if (Character.isDigit(character)) {
+                temp += character;
+            } else {
+                sum += Integer.parseInt(temp);
+                temp = "0";
+            }
+        }
+        return sum + Integer.parseInt(temp);
+    }
+
+
+}
