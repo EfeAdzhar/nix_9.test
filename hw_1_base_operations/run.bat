e70563a945354565f3153b37b2f475243bc12161
EfeAdzhar <efeadzhar@gmail.com>
2021-10-16 14:31:23 +0300

    seventh commit

--- hw_1_base_operations/run.bat
+++ hw_1_base_operations/run.bat
@@ -1,2 +1,2 @@
-call clean install
+call mvn clean install
 call java -jar .\target\hw_01_base_operations.jar
---
+++ hw_1_base_operations/run.sh
@@ -0,0 +1,3 @@
+call mvm clean install
+# shellcheck disable=SC1001
+call java -jar .\target\hw_01_base_operations.jar
